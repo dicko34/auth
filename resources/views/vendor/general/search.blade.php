d44d9c7862108d222703c580fe62455fb16f0547
Djaber-Meghar <djabermeghar@gmail.com>
2023-08-14 10:51:41 +0100

    Update search.blade.php

--- resources/views/vendor/general/search.blade.php
+++ resources/views/vendor/general/search.blade.php
@@ -42,7 +42,6 @@
                         </div>  
                         <div class="col-sm-4">
                             <div class="form-group row">
-                                <label class="col-sm-4 col-form-label"> عنوان</label>
                                 <div class="col-sm-8">
                                     <input class="form-control" type="text" placeholder="مثال: التلفزيون">
                                 </div>
@@ -58,51 +57,6 @@
             </div>
         </div>
     </div>
-
-    {{-- <div class="row">
-        <div class="card w-100">
-            <div class="body-card m-5">
-                <div class="row">
-                    <h3 class="mb-4 col-6"><i class="fas fa-star mr-2"></i> الاعلانات المميزة </h3>
-                    <div class="col-6 text-right">
-                        <a href="{{ route('choseAdd') }}"  class="btn btn-primary w-md waves-effect waves-light" >أضافة أعلان هنا</a>
-                    </div>
-                </div>
-                <div class="row">
-
-                    @for ($i = 0; $i < 4; $i++)
-                        <div class="col-md-6 col-lg-6 col-xl-3">
-                            <div class="card">
-                                <a href="{{ asset('assets/admin/images/martina.jpg') }}" class="gallery-popup"
-                                    style="height: 230px; width:100%">
-                                    <div class="project-item">
-                                        <div class="overlay-container">
-                                            <img src="{{ asset('assets/admin/images/martina.jpg') }}" alt="img"
-                                                class="gallery-thumb-img m-0" style="height: 230px; width:100%">
-                                            <div class="project-item-overlay text-right">
-                                                <h4>عقارات</h4>
-                                                <p>
-                                                    <img src="{{ asset('assets/admin/images/martina.jpg') }}" alt="user"
-                                                        class="avatar-xs rounded-circle">
-                                                    <span class="ml-2">مارتينا جرجس</span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
-                                <div class="card-body">
-                                    <h4 class="card-title">شقق</h4>
-                                    <p class="card-text">شقة عظم للبيع في الخليل ١٣٦ م - فيصل بريك 0562700836
-                                        وسط الخليل على بعد 570 متر من مستشفى الأهلي</p>
-                                </div>
-                            </div>
-                        </div>
-                    @endfor
-
-                </div>
-            </div>
-        </div>
-    </div> --}}
 
     <div class="row">
         <div class="card w-100">
