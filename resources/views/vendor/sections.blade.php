33f37061746a4a742f233391c54acf14f5c00a43
Djaber-Meghar <djabermeghar@gmail.com>
2023-08-23 21:10:18 +0100

    Fixing section Page

--- resources/views/vendor/sections.blade.php
+++ resources/views/vendor/sections.blade.php
@@ -22,31 +22,31 @@
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route("home.index") }}">
+                        <a class="nav-link" href="{{ route("homes.index") }}">
                             <i class="mdi mdi-home mr-2"></i> شقق
                         </a> 
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route("shop.index") }}">
+                        <a class="nav-link" href="{{ route("shops.index") }}">
                             <i class=" mdi mdi-shopping-search mr-2"></i> محلات و مكاتب 
                         </a> 
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route("land.index") }}">
+                        <a class="nav-link" href="{{ route("lands.index") }}">
                             <i class="mdi mdi-view-dashboard mr-2"></i> اراضي
                         </a> 
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route("job.index") }}">
+                        <a class="nav-link" href="{{ route("jobs.index") }}">
                             <i class=" fas fa-address-book mr-2"></i> توظيف
                         </a> 
                     </li> 
 
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route("mobile.index") }}">
+                        <a class="nav-link" href="{{ route("mobiles.index") }}">
                             <i class="mbri-mobile2 mr-2"></i>الاجهزة الذكية
                         </a> 
                     </li>
--- routes/web.php
+++ routes/web.php
@@ -20,41 +20,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/','HomeController@index')->name('home');
 
 // Vendor routes
-
 Route::prefix('vendor')->group(function(){
     Route::get('/', 'Users\Vendor\VendorController@index')->name('vendor.dashboard');
     Route::any('/gallery/{id}', 'Users\Vendor\VendorController@gallery')->name('vendor.gallery');
 
-
-   
-
-});
-Route::group([
-    'as' => 'vendor.',
-    'namespace' => 'Vendor',
-], function(){
-
-    Route::post('cars','CarController@store')->name('cars.store');
-    
-    Route::post('generals','GeneralController@store')->name('generals.store');
-
-    Route::post('homes','HomeController@store')->name('homes.store');
-
-    Route::post('jobs','JobController@store')->name('jobs.store');
-    
-    Route::post('lands','LandController@store')->name('lands.store');
-
-    Route::post('mobiles','MobileController@store')->name('mobiles.store');
-
-    Route::post('shopes','ShopController@store')->name('shopes.store');
-
-    Route::resource('contacts', 'ContactController');
+    Route::get('/login', 'Auth\VendorLoginController@showLoginForm')->name('vendor.login');
+    Route::post('/login', 'Auth\VendorLoginController@login')->name('vendor.login.submit');
+    Route::get('/register', 'Auth\VendorRegisterController@showRegisterForm')->name('vendor.register');
+    Route::post('/register', 'Auth\VendorRegisterController@register')->name('vendor.register.submit');
 
 });
+
 //Auth::routes();
 Route::get('/login', function () {
     return view('vendor.login');
-})->name('login')->middleware('auth:vendor');
+})->name('login');
 
 Route::get('/register', function () {
     return view('vendor.register');
@@ -66,40 +46,43 @@ Route::get('/contact', function () {
 
 
 Route::any('/cars', 'Site\CarController@index')->name('car.index');
-Route::any('/cars/search', 'Site\CarController@search')->name('cars.search');
-Route::any('/car/product/{car}', 'Site\CarController@product')->name('car.product');
-Route::any('/car/add', 'Site\CarController@add')->name('car.add');
+Route::any('/cars/search', 'Site\CarController@search')->name('car.search');
+Route::any('/cars/product', 'Site\CarController@product')->name('car.product');
+Route::any('/cars/add', 'Site\CarController@add')->name('car.add');
 
 Route::any('/choseAdd', 'Site\CarController@choseAdd')->name('choseAdd');
 
-Route::any('/homes', 'Site\HomeController@index')->name('home.index');
+Route::any('/homes', 'Site\HomeController@index')->name('homes.index');
 Route::any('/homes/search', 'Site\HomeController@search')->name('homes.search');
-Route::any('/home/product/{home}', 'Site\HomeController@product')->name('home.product');
-Route::any('/home/add', 'Site\HomeController@add')->name('home.add');
+Route::any('/homes/product', 'Site\HomeController@product')->name('homes.product');
+Route::any('/homes/add', 'Site\HomeController@add')->name('homes.add');
 
-Route::any('/shopes', 'Site\ShopController@index')->name('shop.index');
-Route::any('/shopes/search', 'Site\ShopController@search')->name('shops.search');
-Route::any('/shopes/product/{shop}', 'Site\ShopController@product')->name('shop.product');
-Route::any('/shopes/add', 'Site\ShopController@add')->name('shop.add');
+Route::any('/shops', 'Site\ShopController@index')->name('shops.index');
+Route::any('/shops/search', 'Site\ShopController@search')->name('shops.search');
+Route::any('/shops/product', 'Site\ShopController@product')->name('shops.product');
+Route::any('/shops/add', 'Site\ShopController@add')->name('shops.add');
 
-Route::any('/lands', 'Site\LandsController@index')->name('land.index');
+Route::any('/lands', 'Site\LandsController@index')->name('lands.index');
 Route::any('/lands/search', 'Site\LandsController@search')->name('lands.search');
-Route::any('/land/product/{land}', 'Site\LandsController@product')->name('land.product');
-Route::any('/lands/add', 'Site\LandsController@add')->name('land.add');
+Route::any('/lands/product', 'Site\LandsController@product')->name('lands.product');
+Route::any('/lands/add', 'Site\LandsController@add')->name('lands.add');
 
-Route::any('/jobs', 'Site\JobsController@index')->name('job.index');
+Route::any('/jobs', 'Site\JobsController@index')->name('jobs.index');
 Route::any('/jobs/search', 'Site\JobsController@search')->name('jobs.search');
-Route::any('/job/product/{job}', 'Site\JobsController@product')->name('job.product');
-Route::any('/job/add', 'Site\JobsController@add')->name('job.add');
+Route::any('/jobs/product', 'Site\JobsController@product')->name('jobs.product');
+Route::any('/jobs/add', 'Site\JobsController@add')->name('jobs.add');
 
-Route::any('/mobiles', 'Site\MobilesController@index')->name('mobile.index');
+Route::any('/mobiles', 'Site\MobilesController@index')->name('mobiles.index');
 Route::any('/mobiles/search', 'Site\MobilesController@search')->name('mobiles.search');
-Route::any('/mobile/product/{mobile}', 'Site\MobilesController@product')->name('mobile.product');
-Route::any('/mobile/add', 'Site\MobilesController@add')->name('mobile.add');
+Route::any('/mobiles/product', 'Site\MobilesController@product')->name('mobiles.product');
+Route::any('/mobiles/add', 'Site\MobilesController@add')->name('mobiles.add');
 
-Route::any('/generals', 'Site\GeneralController@index')->name('general.index');
-Route::any('/generals/search', 'Site\GeneralController@search')->name('generals.search');
-Route::any('/general/product/{general}', 'Site\GeneralController@product')->name('general.product');
-Route::any('/general/add', 'Site\GeneralController@add')->name('general.add');
+Route::any('/general', 'Site\generalController@index')->name('general.index');
+Route::any('/general/search', 'Site\generalController@search')->name('general.search');
+Route::any('/general/product', 'Site\generalController@product')->name('general.product');
+Route::any('/general/add', 'Site\generalController@add')->name('general.add');
 
+Route::get('/test', function () {
+    return view('vendor.mobilesnew.add');
+});
 Auth::routes();
